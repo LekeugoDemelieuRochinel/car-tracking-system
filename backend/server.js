767438c379a0c52d0e2a4f15ee4e8766e42768db
kurocifer <draculemihawk232@gmail.com>
2025-01-02 12:55:45 +0100

    setup mogoDB connection

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const http = require('http');
 const { Server } = require('socket.io');
+const connectDB = require('./config/db'); // Adjusted path
 
 dotenv.config();
 
@@ -15,7 +16,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Create a basic Socket.io server
+// Connect to MongoDB
+connectDB();
+
+// Create a basic Socket.IO server
 const server = http.createServer(app);
 const io = new Server(server);
 
@@ -24,7 +28,7 @@ app.get('/', (req, res) => {
     res.send('Car Tracking API is running');
 });
 
-// Socket.io connection
+// Socket.IO connection
 io.on('connection', (socket) => {
     console.log('A user connected');
     socket.on('disconnect', () => {
